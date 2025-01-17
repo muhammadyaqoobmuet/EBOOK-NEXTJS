37ea44a5eaafc763609dec04864e26bea1795293
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-01-17 22:19:08 +0500

    fetch and display book list in Home component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,19 @@
 import Banner from "@/components/Banner";
+import BookList from "./components/BookList";
 
+export default async function Home() {
+  const resp = await fetch(`${process.env.BACKEND_URL}/books`);
+  if (!resp.ok) {
+    throw new Error("An error occurred while fetching data");
+  }
 
-export default function Home() {
+  const books = await resp.json();
+ 
+
   return (
     <>
-    <Banner />
+      <Banner />
+      <BookList books={books.books} />
     </>
   );
 }
