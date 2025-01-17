21fd20d5b4980a72415d27fa127c6d13eb252b3b
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-01-17 23:14:44 +0500

    refactor BookList component to fetch books data asynchronously

--- src/app/(home)/components/BookList.tsx
+++ src/app/(home)/components/BookList.tsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { Book } from "../../../types";
+
 import BookCard from "./BookCard";
+import { Book } from "@/types";
 
-interface BookListProps {
-  books: Book[];
-}
 
-const BookList: React.FC<BookListProps> = ({ books }) => {
+  const resp = await fetch(`${process.env.BACKEND_URL}/books`, {
+    cache: "no-store",
+  });
+  if (!resp.ok) {
+    throw new Error("An error occurred while fetching data");
+  }
+
+
+const data = await resp?.json();
+const books = await data.books;
+
+const BookList = async () => {
   return (
     <div className="mt-5 grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3  gap-8 p-5">
-      {books.map((book) => {
+      {books.map((book: Book) => {
         return <BookCard key={book._id} book={book} />;
       })}
     </div>
