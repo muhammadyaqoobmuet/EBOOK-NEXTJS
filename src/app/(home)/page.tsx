e9dab4eabbe16d651b4d20fb0316314cd9fa4c0b
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-01-17 23:14:35 +0500

    refactor Home component to use Suspense for loading state in BookList

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,15 @@
 import Banner from "@/components/Banner";
 import BookList from "./components/BookList";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 export default async function Home() {
-  const resp = await fetch(`${process.env.BACKEND_URL}/books`);
-  if (!resp.ok) {
-    throw new Error("An error occurred while fetching data");
-  }
-
-  const books = await resp.json();
- 
-
   return (
     <>
       <Banner />
-      <BookList books={books.books} />
+      <Suspense fallback={<Loading/>}>
+        <BookList />
+      </Suspense>
     </>
   );
 }
