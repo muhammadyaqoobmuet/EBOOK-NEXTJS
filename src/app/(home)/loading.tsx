8a735dc9cbe16cf1629b658fdab3305c5980aecd
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-01-17 23:14:17 +0500

    add loading

---
+++ src/app/(home)/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      <h1 className="text-3xl">Home Loading......</h1>
+    </div>
+  );
+}
+
+export default Loading;
