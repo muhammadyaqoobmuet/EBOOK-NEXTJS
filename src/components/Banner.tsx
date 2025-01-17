42299353b0688d860cb3cda7591b360a05b27019
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-01-17 20:33:59 +0500

    add Banner component with image and title styling

---
+++ src/components/Banner.tsx
@@ -0,0 +1,14 @@
+
+import Image from "next/image";
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="rounded-md shadow-black p-5 relative overflow-hidden">
+      <Image alt="book-image" width={1000} height={1}  className="w-screen h-[20rem] rounded-lg shadow-orange-300/30 shadow-md" src="/bookBanner.jpg" />
+      <h1 className="bg-orange-800/80 py-2 text-center text-2xl font-bold tracking-widest rounded-e-full rounded-l-full w-full  text-white left-1/2 -translate-x-1/2  absolute top-1/2 -translate-y-1/2 ">OLD SCHOOL BOOKS</h1>
+    </div>
+  );
+};
+
+export default Banner;
