c9d53b349e55fbd3488c2a64b95e784d59d1b7f2
muhammadyaqoobmuet <yaqoobahmed45700@gmail.com>
2025-01-17 23:14:57 +0500

    add loading to  books/id

---
+++ src/app/books/[id]/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function loading() {
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      <h1 className="text-3xl">Loading......</h1>
+    </div>
+  );
+}
+
+export default loading;
